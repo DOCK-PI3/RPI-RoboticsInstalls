a199a6264c75587b5270eb019e068381e6d4baf3
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2019-11-16 16:48:30 +0000

    Update DOCK-PI3_Roboticsinstall.sh
    rename ...

--- DOCK-PI3_Roboticsinstall.sh
+++ DOCK-PI3_Roboticsinstall.sh
@@ -16,7 +16,7 @@ function main_menu() {
             --ok-label OK --cancel-label Exit \
             --menu "Version: $version - Que accion te gustaria realizar?" 25 75 20 \
             100 "-------------- Para RPI3 ----------------" \
-			0 "Rpi Instalar MasOS-Setup script" \
+			0 "Rpi Instalar EmuCOPS NOOBs v1 FullSetup script" \
 			1 "Rpi Instalar MasOS-Setup script" \
 			2 "Rpi Instalar EmulOS-Setup script" \
 			3 "Rpi Instalar PiVPN" \
