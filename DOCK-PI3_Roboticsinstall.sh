e0e56c897df8ef45004e83dc6de5e590433ee998
Dock.pi3 <41844169+DOCK-PI3@users.noreply.github.com>
2019-11-19 01:18:11 +0000

    Update DOCK-PI3_Roboticsinstall.sh
    performance all RPI4 scripts instalators..

--- DOCK-PI3_Roboticsinstall.sh
+++ DOCK-PI3_Roboticsinstall.sh
@@ -265,8 +265,7 @@ function masos_instalador() {
 dialog --infobox "... Script instalador de MasOS en su version mas reciente ..." 30 55 ; sleep 3
 sudo apt-get update
 sudo apt-get install -y git
-cd
-git clone --depth=1 https://github.com/DOCK-PI3/MasOS-Setup.git
+cd && git clone --depth=1 https://github.com/DOCK-PI3/MasOS-Setup.git
 sudo chmod -R +x MasOS-Setup/
 cd MasOS-Setup
 sudo ./masos_setup.sh
@@ -276,8 +275,7 @@ function emulos_instalador() {
 dialog --infobox "... Script instalador de EmulOS en su version mas reciente ...\n\n" 30 55 ; sleep 3
 sudo apt-get update
 sudo apt-get install -y git
-cd
-git clone --depth=1 https://github.com/Moriggy/EmulOS-Setup.git
+cd && git clone --depth=1 https://github.com/Moriggy/EmulOS-Setup.git
 cd EmulOS-Setup
 sudo ./emulos_setup.sh
 }
@@ -364,7 +362,7 @@ dialog --infobox "... RetroArch 1.8.1 instalado correctamente en su rpi3! ..." 3
 
 function RPI4_retroarch_instalador() {                                          
 dialog --infobox "... RIP4 BETA Script instalador de Retroarch en su version 1.8.1 ..." 30 55 ; sleep 3
-sudo apt update
+sudo apt-get update
 #dialog --infobox "... Iniciando actualizacion del sistema y sus paquetes ,comentado dmomento..." 30 55 ; sleep 2
 # sudo apt upgrade -y
 #dialog --infobox "... Elija la distribucion para su teclado ..." 30 55 ; sleep 5
@@ -374,21 +372,21 @@ sudo apt update
 #dialog --infobox "... Elija su zona horaria ..." 30 55 ; sleep 5
 #sudo dpkg-reconfigure tzdata
 dialog --infobox "... Compilar e instalar RetroArch ,iniciando espere! ..." 30 55 ; sleep 5
-sudo apt-get install -y build-essential libasound2-dev libudev-dev libgles2-mesa-dev
-#sudo apt-get install -y build-essential libxkbcommon-dev zlib1g-dev libfreetype6-dev libegl1-mesa-dev libgles2-mesa-dev libgbm-dev nvidia-cg-toolkit nvidia-cg-dev libavcodec-dev libsdl2-dev libsdl-image1.2-dev libxml2-dev yasm
+#sudo apt-get install -y build-essential libasound2-dev libudev-dev libgles2-mesa-dev
+sudo apt-get install -y build-essential libxkbcommon-dev zlib1g-dev libfreetype6-dev libegl1-mesa-dev libasound2-dev libudev-dev libgles2-mesa-dev libgles2-mesa-dev libgbm-dev nvidia-cg-toolkit nvidia-cg-dev libavcodec-dev libsdl2-dev libsdl-image1.2-dev libxml2-dev yasm
 cd && curl -LO 'https://github.com/libretro/RetroArch/archive/v1.8.1.tar.gz' && tar -zxvf v1.8.1.tar.gz
 sudo rm v1.8.1.tar.gz
 cd RetroArch-1.8.1
 CFLAGS='-mfpu=neon -mtune=cortex-a72 -march=armv8-a' ./configure --disable-opengl1 --enable-neon --enable-opengles3 --enable-opengles --disable-videocore
-make
+make -j4
 sudo make install
 cd && sudo rm -R RetroArch-1.8.1/
 
 ##### instalar bios base
 dialog --infobox "... Descargando y Copiando BIOS BASE para retroarch en /home/pi/.config/retroarch/system ..." 30 55 ; sleep 3
 
-cd && git clone --depth 1 https://github.com/DOCK-PI3/rpi-retroarch-bios.git
-cp -R rpi-retroarch-bios/system/ /home/pi/.config/retroarch/
+cd && git clone https://github.com/DOCK-PI3/rpi-retroarch-bios.git
+cd && cp -R rpi-retroarch-bios/system/ /home/pi/.config/retroarch/
 sudo rm -R /home/pi/rpi-retroarch-bios/
 
 dialog --infobox "... Descarga de BIOS BASE: correcta ..." 30 55 ; sleep 3
@@ -458,8 +456,8 @@ dialog --infobox " Attract se instalo de forma correcta y con mmal ... " 350 350
 function RPI4_attractmode_instalador() {                                          
 dialog --infobox "... RPI4 Script instalador de AttractMode en su version mas reciente ..." 30 55 ; sleep 3
 # Cierra ES para una mejor y mas rapida compilacion de attract y ffmpeg......
-sudo killall emulationstation
-sudo killall emulationstation-dev
+# sudo killall emulationstation
+# sudo killall emulationstation-dev
 
 # ACTUALIZAR LISTA DE PAQUETES
 sudo apt-get update
@@ -468,7 +466,7 @@ sudo apt-get update
 cd /home/pi && mkdir develop
 
 # Instalar las dependencias para "sfml-pi" y Attract-Mode
-sudo apt-get install -y git-core make cmake pkg-config libflac-dev libogg-dev libvorbis-dev libopenal-dev libfreetype6-dev libudev-dev libjpeg-dev libudev-dev libfontconfig1-dev
+sudo apt-get install -y git-core make cmake pkg-config libflac-dev libogg-dev libvorbis-dev libopenal-dev libjpeg-dev libjpeg8-dev libfreetype6-dev libudev-dev libudev-dev libfontconfig1-dev
 sudo apt-get install -y libx11-dev libx11-xcb-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-icccm4-dev libxrandr2 libxrandr-dev libgles2-mesa-dev
 
 # Descargar y compilar sfml-pi
@@ -479,7 +477,8 @@ sudo apt-get install -y libx11-dev libx11-xcb-dev libxcb-randr0-dev libxcb-image
 # sudo make -j4 install
 # sudo ldconfig
 
-#sudo apt-get install -y libsfml-dev
+sudo apt-get install -y libsfml-dev
+sudo ldconfig
 
 # Compilar FFmpeg con soporte mmal (decodificacion de video acelerada por hardware)
 cd /home/pi/develop
@@ -493,14 +492,17 @@ sudo ldconfig
 # Descargar y compilar Attract-Mode
 cd && mkdir .attract
 cd /home/pi/develop
-git clone --depth 1 https://github.com/mickelson/attract attract
-cd attract && cp -R config/* /home/pi/.attract/
-make -j4 USE_GLES=1
-sudo make -j4 install USE_GLES=1
+#git clone --depth 1 https://github.com/mickelson/attract attract
+git clone https://github.com/mickelson/attract attract
+cd attract
+make -j3 USE_GLES=1
+sudo make -j3 install USE_GLES=1
 sudo rm -r -f /home/pi/develop
+dialog --infobox " Ahora se abre attract mode, una vez que inicie attract seleccione su idioma \n y luego cierre atrract mode para seguir con la configuracion. " 350 350 ; sleep 10
+attract
 
 #### config full rescue ######
-cd && git clone --deep 1 https://github.com/DOCK-PI3/EmuCOPS-Attract-autoconf.git
+cd && git clone https://github.com/DOCK-PI3/EmuCOPS-Attract-autoconf.git
 cp -R /home/pi/EmuCOPS-Attract-autoconf/attract/* /home/pi/.attract/
 cd && mkdir EmuCOPS
 cp -R /home/pi/EmuCOPS-Attract-autoconf/EmuCOPS/* /home/pi/EmuCOPS/
@@ -522,32 +524,32 @@ cd EmUCoP-cores
 dialog --infobox "... RPI4 Retroarch instalando core libretro-fceumm ..." 30 55 ; sleep 2
 git clone --depth 1 https://github.com/libretro/libretro-fceumm.git
 cd libretro-fceumm
-make -j4
+make -j3
 
 dialog --infobox "... RPI4 Retroarch instalando core snes9x2010 ..." 30 55 ; sleep 2
 cd ~
 cd EmUCoP-cores
 git clone --depth 1 https://github.com/libretro/snes9x2010.git
 cd snes9x2010
-make -j4
+make -j3
 
 dialog --infobox "... RPI4 Retroarch instalando core mupen64plus-libretro ..." 30 55 ; sleep 2
 cd ~
 cd EmUCoP-cores
 git clone --depth 1 https://github.com/libretro/mupen64plus-libretro.git
 cd mupen64plus-libretro
-platform=rpi4 make -j4
+platform=rpi4 make -j3
 
 dialog --infobox "... RPI4 Retroarch instalando core  pcsx_rearmed ..." 30 55 ; sleep 2
 cd ~
 cd EmUCoP-cores
 git clone --depth 1 https://github.com/libretro/pcsx_rearmed.git
 cd pcsx_rearmed
-platform=rpi4 make -j4 -f Makefile.libretro
+platform=rpi4 make -f Makefile.libretro
 
 dialog --infobox "... Descargando y Copiando mas de 70 cores para retroarch en /home/pi/.config/retroarch/cores ..." 30 55 ; sleep 5
-cd && git clone --depth 1 https://github.com/DOCK-PI3/LR-CORES-RPI4.git
-cp -R LR-CORES-RPI4/*.so /home/pi/.config/retroarch/cores
+cd && git clone https://github.com/DOCK-PI3/LR-CORES-RPI4.git
+cd && cp -R LR-CORES-RPI4/*.so /home/pi/.config/retroarch/cores
 
 cd && cp -R EmUCoP-cores/libretro-fceumm/*.so /home/pi/.config/retroarch/cores
 cd && cp -R EmUCoP-cores/snes9x2010/*.so /home/pi/.config/retroarch/cores
@@ -561,8 +563,8 @@ sudo rm -R /home/pi/LR-CORES-RPI4/
 
 function vsftpd_instalador() {                                          
 dialog --infobox "... Script instalador de VsFTPd en su version mas reciente ..." 30 55 ; sleep 3
-sudo apt update
-sudo apt install -y vsftpd
+sudo apt-get update
+sudo apt-get install -y vsftpd
 # Agregar primer usuario para el ftp
 dialog --infobox "... Vamos a crear 1 usuario para el ftp,de nombre masosteam\n el pasword escribalo ahora y pulse enter y enter para saltar lo demas..." 40 55 ; sleep 7
 sudo adduser masosteam
